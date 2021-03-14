cd0f547182073a2d4ae429eda14576fbc15f8f0c
Adrian Reyes <sadrian.reyes@gmail.com>
2021-03-14 18:48:52 +0100

    ADD Travis experimental

---
+++ ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 0
+fi
+
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
+TAG="${TRAVIS_TAG:-latest}"
+docker buildx build \
+     --progress plain \
+    --platform=linux/amd64,linux/arm64,linux/arm/v7 \
+    -t $DOCKER_REPO:$TAG \
+    --push \
+    .
