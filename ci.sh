369ad6cc2f1db86c81775edb85760faefba2f4c1
Adrian Reyes <sadrian.reyes@gmail.com>
2021-03-14 18:58:22 +0100

    REMOVE progress from docker build

--- ci.sh
+++ ci.sh
@@ -8,7 +8,6 @@ echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
-     --progress plain \
     --platform=linux/amd64,linux/arm64,linux/arm/v7 \
     -t $DOCKER_REPO:$TAG \
     --push \
